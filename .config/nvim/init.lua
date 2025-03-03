5fa32164377e5eef751ef585e015fb0e10e060df
Conner Holden <80974257+cloud303-cholden@users.noreply.github.com>
2025-03-03 00:27:02 -0700

    Add nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,13 @@ local o = vim.opt
 local g = vim.g
 
 o.ignorecase = false
-o.clipboard = "unnamedplus"
+o.expandtab = true
 o.fillchars = { eob = " " }
 o.number = true
-o.statuscolumn = "%l  "
-o.smartcase = true
+o.shiftwidth = 2
+o.signcolumn = "yes"
+o.smartcase = false
 o.smartindent = true
-o.smarttab = true
-o.expandtab = true
 o.tabstop = 2
 o.timeoutlen = 200
 o.undofile = true
@@ -20,9 +19,12 @@ g.netrw_winsize = 15
 
 vim.cmd [[
   inoremap jk <Esc>
-  nnoremap <A-e> :Lexplore %:p:h<CR>
+  nnoremap <A-e> :lua MiniFiles.open()<CR>
   nnoremap <A-w> :w<CR>
   nnoremap <A-q> :qa<CR>
+  nnoremap gd :lua vim.lsp.buf.definition()<CR>
+  nnoremap gj :lua vim.diagnostic.goto_next({buffer=0})<CR>
+  nnoremap gk :lua vim.diagnostic.goto_prev({buffer=0})<CR>
   vnoremap jk <Esc>
   vnoremap . >gv
   vnoremap , <gv
@@ -31,15 +33,15 @@ vim.cmd [[
   inoremap <C-v> <C-o>"+p
 ]]
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "netrw" },
+vim.diagnostic.config({
+  signs = false,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
   callback = function()
-    vim.cmd [[
-      nmap <buffer> l <CR>
-      nmap <buffer> L <CR>:Lexplore<CR>
-    ]]
+    vim.lsp.buf.format({ async = false })
   end,
 })
 
 require("plugins")
-
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,16 @@
 local plugins = {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        autopairs = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+
   {
     "gbprod/nord.nvim",
     lazy = false,
@@ -11,15 +22,38 @@ local plugins = {
       vim.cmd.colorscheme("nord")
     end,
   },
-  {"williamboman/mason.nvim", opts = {
-    ui = { border = "rounded" },
-  }},
-  {"williamboman/mason-lspconfig.nvim", opts = {
-    ensure_installed = {"lua_ls", "rust_analyzer"},
-  }},
-  {"neovim/nvim-lspconfig",
+  { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-pack/nvim-spectre",
     config = function()
-      require("lspconfig").lua_ls.setup({
+      require("spectre").setup({
+        live_update = true,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "spectre_panel" },
+        callback = function()
+          vim.wo.number = false
+        end,
+      })
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = { border = "rounded" },
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp = require("lspconfig")
+      lsp.lua_ls.setup({
         settings = {
           Lua = {
             diagnostics = {
@@ -28,10 +62,88 @@ local plugins = {
           },
         },
       })
-      require("lspconfig").rust_analyzer.setup({})
+      lsp.rust_analyzer.setup({})
+      lsp.svelte.setup({})
     end
   },
-  {"tzachar/highlight-undo.nvim", config = true},
+  { "lewis6991/gitsigns.nvim", config = true },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        section_separators = '',
+        component_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'diff', { 'diagnostics', padding = { left = 0, right = 1 } } },
+        lualine_y = { { 'branch', icons_enabled = false, color = { gui = "italic,bold" } } },
+        lualine_z = {},
+      },
+    }
+  },
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+    config = function()
+      require("mini.files").setup()
+      require("mini.pick").setup()
+      require("mini.completion").setup()
+      require("mini.cursorword").setup()
+      require("mini.indentscope").setup()
+      require("mini.notify").setup()
+      require("mini.surround").setup()
+      local miniclue = require('mini.clue')
+      miniclue.setup({
+        window = {
+          config = {
+            width = 40,
+          },
+        },
+        triggers = {
+          -- Leader triggers
+          { mode = 'n', keys = '<Leader>' },
+          { mode = 'x', keys = '<Leader>' },
+
+          -- Built-in completion
+          { mode = 'i', keys = '<C-x>' },
+
+          -- `g` key
+          { mode = 'n', keys = 'g' },
+          { mode = 'x', keys = 'g' },
+
+          -- Marks
+          { mode = 'n', keys = "'" },
+          { mode = 'n', keys = '`' },
+          { mode = 'x', keys = "'" },
+          { mode = 'x', keys = '`' },
+
+          -- Registers
+          { mode = 'n', keys = '"' },
+          { mode = 'x', keys = '"' },
+          { mode = 'i', keys = '<C-r>' },
+          { mode = 'c', keys = '<C-r>' },
+
+          -- Window commands
+          { mode = 'n', keys = '<C-w>' },
+
+          -- `z` key
+          { mode = 'n', keys = 'z' },
+          { mode = 'x', keys = 'z' },
+        },
+        clues = {
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      })
+    end
+  },
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -41,7 +153,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -56,4 +168,3 @@ require("lazy").setup({
   checker = { enabled = true },
   rocks = { enabled = false },
 })
-
