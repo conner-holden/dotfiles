55e2e239f0b4d1b648cab2f028a4a8ceb52faf68
Conner Holden <80974257+cloud303-cholden@users.noreply.github.com>
2025-03-17 13:49:19 -0600

    Add search/replace, remove vim, improve config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -93,6 +93,19 @@ local plugins = {
     end
   },
   {
+    "MagicDuck/grug-far.nvim",
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require("grug-far").setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' or 'astgrep-rules' can
+        -- be specified
+      });
+    end
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ui = { border = "rounded" },
