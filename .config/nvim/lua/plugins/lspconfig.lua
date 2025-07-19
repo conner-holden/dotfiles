0fc67f7f2658467d03d7a363e954dedb989450d7
Conner Holden <200126914+conner-holden@users.noreply.github.com>
2025-07-19 10:43:43 -0600

    Add Python LSP config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -13,10 +13,40 @@ return {
         },
       },
     })
+    lsp.pyright.setup({
+      settings = {
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { '*' },
+          },
+        },
+      },
+    })
+    lsp.ruff.setup({ capabilities = capabilities })
     lsp.svelte.setup({ capabilities = capabilities })
     lsp.vtsls.setup({ capabilities = capabilities })
     lsp.tailwindcss.setup({ capabilities = capabilities })
     lsp.terraformls.setup({ capabilities = capabilities })
     lsp.tflint.setup({})
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+          return
+        end
+        if client.name == 'ruff' then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end,
+      desc = 'LSP: Disable hover capability from Ruff',
+    })
   end,
 }
