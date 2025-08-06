f71cc03983b9589eaa0dc1849cd1a4edffc967b1
Conner Holden <200126914+conner-holden@users.noreply.github.com>
2025-08-06 11:42:17 -0600

    Add blink rg support

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'mikavilpas/blink-ripgrep.nvim',
+  },
   version = '1.*',
   opts = {
     keymap = { preset = 'default' },
@@ -9,7 +12,36 @@ return {
     },
     completion = { documentation = { auto_show = false } },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'buffer', 'ripgrep', 'snippets' },
+      providers = {
+        ripgrep = {
+          module = 'blink-ripgrep',
+          name = 'Ripgrep',
+          opts = {
+            prefix_min_len = 3,
+            project_root_marker = '.git',
+            fallback_to_regex_highlighting = true,
+            toggles = {
+              on_off = nil,
+              debug = nil,
+            },
+            backend = {
+              use = 'ripgrep',
+              customize_icon_highlight = true,
+              ripgrep = {
+                context_size = 5,
+                max_filesize = '1M',
+                project_root_fallback = true,
+                search_casing = '--ignore-case',
+                additional_rg_options = {},
+                ignore_paths = {},
+                additional_paths = {},
+              },
+            },
+            debug = false,
+          },
+        },
+      },
     },
     fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
